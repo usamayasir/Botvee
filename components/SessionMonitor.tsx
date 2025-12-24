f6a2746f8152058dfa5317fe4eea99403b2841d6
Usama Yasir <59642493+usamayasir@users.noreply.github.com>
2025-12-24 21:15:42 +0500

    Delete components directory

--- components/SessionMonitor.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-import { useSessionValidation } from '@/hooks/useSessionValidation';
-
-export default function SessionMonitor() {
-  // This hook handles all session validation and logout logic
-  useSessionValidation();
-  
-  return null; // This component doesn't render anything
-}
